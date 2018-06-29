98895788cdcd7a601344468cefde240e6ccfbd6c
Bruce Lucas <bruce.lucas@10gen.com>
2018-06-29 10:52:17 -0400

    Add thickness adjustment

--- models/vase/model.js
+++ models/vase/model.js
@@ -1,11 +1,12 @@
 detail = ui.slider({name: 'detail', min: 10, max: 50, value: 20})
+thickness = ui.slider({name: 'thickness', min: 0.01, max: 0.1, step: 0.005, value: 0.02})
 
 ui.view({distance: 10, height: 2.5})
 
 vase = lib.vase({
     slices: detail,
     wedges: detail,
-    thickness: () => 0.02,
+    thickness: () => thickness,
     radius: (s) => lib.wave(0.0, 0.9, 0.25, .4)(s) - 0.1 * s,
     base: 0.1
 })
