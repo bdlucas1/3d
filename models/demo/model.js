24655d89d2cbfb64c4f3a7cc230e28237afc16c5
Bruce Lucas <bruce.lucas@10gen.com>
2018-06-29 14:04:17 -0400

    Spiff up the demo

--- models/demo/model.js
+++ models/demo/model.js
@@ -1,10 +1,13 @@
 ui.view({distance: 5, height: 4.5})
 
-var a = CSG.cube({radius: 1.1})
-var b = CSG.sphere({radius: 1.35})
-//var c = CSG.sphere({radius: 0.5})
-var c = CSG.cylinder({radius: 0.5})
-//a.setColor(1, 1, 0)
-//b.setColor(0, 0.5, 1)
-c.setColor(.7, 0, .8)
-return {demo: a.subtract(b).union(c)}
+cube = CSG.cube({radius: 1.1})
+sphere = CSG.sphere({radius: 1.35})
+cylinder = CSG.cylinder({radius: 0.5})
+cylinder.setColor(.7, 0, .8)
+
+return {
+    demo: cube.subtract(sphere).union(cylinder),
+    cube,
+    sphere,
+    cylinder
+}
