4d0424c62dc0b227003b824ad4f7d9afedce05ae
Bruce Lucas <bdlucas1@ukulele.home>
2018-07-04 10:21:17 -0400

    Switch to curve()

--- models/vase with holes/model.js
+++ models/vase with holes/model.js
@@ -1,24 +1,28 @@
 const detail = ui.slider({name: 'detail', min: 12, max: 50, value: 20})
 const thickness = ui.slider({name: 'thickness', min: 0.01, max: 0.1, step: 0.005, value: 0.02})
 
+// const f = (s) => lib.wave(0.0, 0.9, 0.25, .4)(s) - 0.1 * s
+const diameter = ui.slider({name: 'vase diameter', min: 0.1, max: 2, step: 0.01, value: 1.05})
+const pts = [{x: 0, y: 0.4}, {x: 0.25, y: 0.523}, {x: 0.5, y: 0.396}, {x: 0.75, y: 0.191}, {x: 1, y: 0.212}]
+const profile = ui.curve({name: 'profile', kind: 'hybrid', value: pts, scale: diameter / 2})
+
 const nh = ui.slider({name: 'n horizontal', min: 1, max: 20, value: 7})
 const nv = ui.slider({name: 'n vertical', min: 1, max: 20, value: 9})
 const hskew = ui.slider({name: 'h skew', min: -0.5, max: 0.5, step: 0.01, value: 0.1})
 const vmin = ui.slider({name: 'v min', min: -1.0, max: 1.0, step: 0.01, value: -0.65})
 const vmax = ui.slider({name: 'v max', min: -1.0, max: 1.0, step: 0.01, value: 0.55})
-const diameter = ui.slider({name: 'hole diameter', min: 0, max: 0.5, step: 0.01, value: 0.2})
-
-const radius = (s) => lib.wave(0.0, 0.9, 0.25, .4)(s) - 0.1 * s
-let maxr = 0
-for (var s = 0; s <= 1; s += 0.01)
-    if (radius(s) > maxr)
-        maxr = radius(s)
-log('maxr', maxr)
-maxr *= 1.1
+const hole = ui.slider({name: 'hole diameter', min: 0, max: 0.5, step: 0.01, value: 0.2})
 
 ui.view({distance: 10, height: 2.5})
 
 punch = lib.empty()
+
+let maxr = 0
+for (var s = 0; s <= 1; s += 0.01)
+    if (profile(s) > maxr)
+        maxr = profile(s)
+log('maxr', maxr)
+maxr *= 1.1
 
 for (var h = 0; h < nh; h++) {
     for (var v = 0; v < nv; v++) {
@@ -29,7 +33,7 @@ for (var h = 0; h < nh; h++) {
         cone = lib.cone({
             base: [x, y, z],
             tip: [0.0*x, y, 0.0*z],
-            radius: diameter / 2 * maxr,
+            radius: hole / 2 * maxr,
             slices: 1,
             wedges: detail / 3
         })
@@ -41,7 +45,7 @@ vase = lib.vase({
     slices: detail,
     wedges: detail,
     thickness: () => thickness,
-    radius: radius,
+    radius: profile,
     base: thickness
 })
 
--- models/vase/model.js
+++ models/vase/model.js
@@ -1,5 +1,6 @@
 detail = ui.slider({name: 'detail', min: 10, max: 50, value: 20})
 thickness = ui.slider({name: 'thickness', min: 0.01, max: 0.1, step: 0.005, value: 0.02})
+
 // const f = (s) => lib.wave(0.0, 0.9, 0.25, .4)(s) - 0.1 * s
 diameter = ui.slider({name: 'diameter', min: 0.1, max: 2, step: 0.01, value: 1.05})
 pts = [{x: 0, y: 0.4}, {x: 0.25, y: 0.523}, {x: 0.5, y: 0.396}, {x: 0.75, y: 0.191}, {x: 1, y: 0.212}]
